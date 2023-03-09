70fb3e7b1a746a3d4f6c2b6038fcf40627b96289
Zinal-Sharma <114439684+Zinal-Sharma@users.noreply.github.com>
2023-03-09 00:28:20 -0500

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,25 @@
+
+URL = 'https://upadhayay.github.io/db.json'
+
+fetch(URL)
+  // get the JSON data
+  .then(response => response.json())
+  // display the JSON data
+  .then(data => {
+    console.log(data)
+    var html = '';
+    for (let i = 0; i < data.books.length; i++) {
+        if(data.books[i].published === true)
+        {
+            html += '<div id="paper1">'+
+            '<div class="paper-img">'+
+                '<img src="images/paper1.jpg" style="height: 300px;">'+
+            '</div>'+
+            '<a href="#" id="title1"><span>'+ data.books[i].title+'</span></a>'+
+            '<br><span class="year">'+ data.books[i].year+'</span>'+
+            '</div>';
+        }
+        document.getElementById('paper-details').innerHTML = html;
+        }
+})
+ 
